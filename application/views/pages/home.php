487e71ed3683931ae0d8dbed07f8c9b2e9dae9bd
adgedenkers <adge.denkers@gmail.com>
2019-08-23 14:22:13 -0400

    updated home.php

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,7 +1,7 @@
         <!-- jumbotron -->
         <div class="jumbotron">
             <div class="container">
-                <img style="margin-top:75px;" src="/hcm_design/images/va_seal.gif">
+                <img style="margin-top:px;" src="/hcm_design/images/va_seal.gif">
 
                 <h1 class="display-3">Human Capital Management Web Apps</h1>
                 <p>OIT Human Capital Management (HCM) provides a series of web applications to our staff and our customers, to provide faster, simpler, and more accurate services. </p>
